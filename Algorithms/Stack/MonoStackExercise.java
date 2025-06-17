1f280f534240a4e5addd494a02470f820ec14fa1
Mahim <80307451+MM120-i@users.noreply.github.com>
2025-06-16 23:17:57 -0400

    Update MonoStackExercise.java

--- Algorithms/Stack/MonoStackExercise.java
+++ Algorithms/Stack/MonoStackExercise.java
@@ -12,6 +12,69 @@ import java.util.*;
 public class MonoStackExercise {
 	
 	/**
+	 * Largest Rectangle in Histogram from leetcode
+	 * 
+	 * Given an integer array heights representing the heights of histogram bars, 
+	 * write a function to find the largest rectangular area possible in a histogram, where each bar's width is 1.
+	 */
+	public static int largestRectangleArea(int[] heights) {
+		
+		Stack<Integer> stack = new Stack<>();
+		int maxArea = 0, i = 0;
+		
+		while(i < heights.length) {
+			
+			if(stack.isEmpty() || heights[i] >= heights[stack.peek()]) {
+				stack.push(i++);
+			}
+			else {
+				int top = stack.pop();
+				int left = stack.isEmpty() ? - 1 : stack.peek();
+				int width = i - left - 1;
+				int area = heights[top] * width;
+				maxArea = Math.max(maxArea, area);
+			}			
+			
+		}
+		
+		while(!stack.isEmpty()) {
+			int top = stack.pop();
+			int left = stack.isEmpty() ? - 1 : stack.peek();
+			int width = i - left - 1;
+			int area = heights[top] * width;
+			maxArea = Math.max(maxArea, area);
+		}
+		
+		return maxArea;
+	}
+	
+	/**
+	 * Daily Temperatures from leetcode
+	 * 
+	 * Given an integer array temps representing daily temperatures, write a function to calculate 
+	 * the number of days one has to wait for a warmer temperature after each given day. 
+	 * The function should return an array answer where answer[i] represents the wait time for a warmer day after the ith day. 
+	 * If no warmer day is expected in the future, set answer[i] to 0.
+	 */
+	public static int[] dailyTemp(int[] temps) {
+		
+		int[] result = new int[temps.length];
+		Stack<Integer> stack = new Stack<>();
+		
+		for(int i = 0; i < temps.length; i++) {
+			
+			while(!stack.isEmpty() && temps[i] > temps[stack.peek()]) {
+				int index = stack.pop();
+				result[index] = i - index;
+			}
+			
+			stack.push(i);
+		}
+		
+		return result;
+	}
+	
+	/**
 	 * Next greater element
 	 * 
 	 * For each element in the array, find the first greater element to its right. If none exists, use -1.
@@ -23,15 +86,14 @@ public class MonoStackExercise {
 		
 		int[] result = new int[nums.length];
 		Stack<Integer> stack = new Stack<>();
-		
 		Arrays.fill(result, -1);
-		
+				
 		for(int i = 0; i < nums.length; i++) {
 			
 			// While the stack isnt empty and the current number is greater than the peek's value
 			while(!stack.isEmpty() && nums[i] > nums[stack.peek()]) {
-				int idx = stack.pop();
-				result[idx] = nums[i];
+				int index = stack.pop();
+				result[index] = nums[i];
 			}
 			
 			stack.push(i);
@@ -68,11 +130,18 @@ public class MonoStackExercise {
 		
 		// next greater element
 		int[] x = {2, 1, 3, 2, 4, 3};
-		int[] greater = nextGreaterElement(x);
-		System.out.println("Greater: " + Arrays.toString(greater));
+		System.out.println("Greater: " + Arrays.toString(nextGreaterElement(x)));
 		
 		// next smaller element
-		int[] smaller = nextSmallerElement(x);
-		System.out.println("Smaller: " + Arrays.toString(smaller));
+		System.out.println("Smaller: " + Arrays.toString(nextSmallerElement(x)));
+		
+		// Daily temp
+		int[] temps = {65, 70, 68, 60, 55, 75, 80, 74};
+		System.out.println(Arrays.toString(dailyTemp(temps)));
+		
+		// Largest area histogram
+		int[] heights = {2, 8, 5, 6, 2, 3};
+		System.out.println("Largest rectangle area: " + largestRectangleArea(heights));
 	}
+
 }
