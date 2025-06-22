9d4d9b55e402bd983fdc487dbd6a2e2dd8d3eb99
Mahim <80307451+MM120-i@users.noreply.github.com>
2025-06-21 20:07:52 -0400

    Update Trie.h

--- Data Structures/Trie/Trie.h
+++ Data Structures/Trie/Trie.h
@@ -1,3 +1,5 @@
+#include <stdbool.h>
+
 #pragma once
 
 #define ALPHABET_SIZE 26
@@ -8,8 +10,16 @@ typedef struct TrieNode
     bool isEndOfWord;
 } TrieNode;
 
-TrieNode *createNode();
+// function headers
+TrieNode *createNode(void);
 void insert(TrieNode *, const char *);
 bool search(TrieNode *, const char *);
 bool deleteHelper(TrieNode *, const char *, int);
 void delete(TrieNode *, const char *);
+static char *printMessage(bool);
+static void DFS(TrieNode *, char *, int, char **, int *);
+char **match(TrieNode *, const char *, int *);
+
+// other function headers
+void freeTrie(TrieNode *);
+void sortResults(char **, int);
